15bb08f7d380eae90b51781f5fafcd8414c57a6b
jinyh <jinyh@19pay.com.cn>
2020-08-04 14:35:11 +0800

    springboot 异步调用

--- simple/src/main/java/com/touchdreams/simple/configuration/TaskExecutorConfig.java
+++ simple/src/main/java/com/touchdreams/simple/configuration/TaskExecutorConfig.java
@@ -3,12 +3,14 @@ package com.touchdreams.simple.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * 线程池的配置
  * Created by jinyh on 2020-8-3.
  */
+@EnableAsync
 @Configuration
 public class TaskExecutorConfig {
 
